a9a1139a97193941abe8c25965e0e777b14c5217
Dante Reinaudo <dantereinaudo@hotmail.com>
2023-04-04 18:45:33 -0300

    Welcome

--- FiuFit/App.js
+++ FiuFit/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo</Text>
+      <Text style = {styles.titulo}> Bienvenido</Text>
+      <Text style = {styles.titulo}> Inicia sesión con tu cuenta </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'tomato',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo :{
+    fontSize:30
+  }
 });
