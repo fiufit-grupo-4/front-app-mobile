2a59191cade70a60b7b034f72a72c3be3f7a1d37
Dante Reinaudo <dantereinaudo@hotmail.com>
2023-04-06 12:55:01 -0300

    Refactor on screen login

--- FiuFit/App.js
+++ FiuFit/App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Dimensions } from 'react-native';
-import Svg, {Path,Defs,LinearGradient,Stop} from 'react-native-svg';
+import { StyleSheet, Text, View, TextInput, Dimensions,TouchableOpacity } from 'react-native';
+import { Input,Icon } from 'react-native-elements';
+import Svg, {Path,Defs,Stop} from 'react-native-svg';
 import {Ionicons} from 'react-native-vector-icons';
+import {LinearGradient} from "expo-linear-gradient"
 import ButtonGradient from "./components/ButtonGradient"
+import { color } from 'react-native-elements/dist/helpers';
 
 const {width,height} = Dimensions.get("window")
 
@@ -29,10 +32,43 @@ export default function App() {
         placeholder='Contraseña'
         style={styles.input}
         secureTextEntry = { true }
+        rightIcon = {
+          <Icon
+            type = "material-community"
+            name = "eye-outline"
+          />
+        }
       />
 
       <Text style = {styles.forgotPassword}> ¿Olvido su Contraseña? </Text>
-      <ButtonGradient/>
+      
+      <TouchableOpacity style= {styles_gradient.container}>
+            <LinearGradient
+                // Button Linear Gradient
+                colors={['black', 'black']}
+                style={styles_gradient.button}>
+                <Text style={styles_gradient.text}> Iniciar sesión </Text> 
+            </LinearGradient>
+      </TouchableOpacity>
+
+      <TouchableOpacity style= {styles_gradient.container}>
+            <LinearGradient
+                // Button Linear Gradient
+                colors={['#3b5998', '#3b5998']}
+                style={styles_gradient.button}>
+                <Text style={styles_gradient.text}> Iniciar sesión con Facebook <Ionicons name="logo-facebook" size={15} color="white" /> </Text> 
+            </LinearGradient>
+      </TouchableOpacity>
+
+      <TouchableOpacity style= {styles_gradient.container}>
+            <LinearGradient
+                // Button Linear Gradient
+                colors={['crimson', 'crimson']}
+                style={styles_gradient.button}>
+                <Text style={styles_gradient.text}> Iniciar sesión con Google <Ionicons name="logo-google" size={15} color="white" /> </Text> 
+            </LinearGradient>
+      </TouchableOpacity>
+      
       <StatusBar style="auto" />
 
       <Text style = {styles.forgotPassword}> Crear una cuenta </Text>
@@ -62,7 +98,7 @@ const styles = StyleSheet.create({
     fontSize:20,
     color : "white",
     fontWeight: "bold",
-    marginTop : 10
+    marginTop : 10,
   },
   input:{
     borderWidth : 1,
@@ -78,6 +114,32 @@ const styles = StyleSheet.create({
     fontSize:20,
     color : "black",
     fontWeight: "bold",
-    marginTop : 20
+    marginTop : 20,
+    marginBottom: 10,
+  }
+});
+
+const styles_gradient = StyleSheet.create({
+  container : {
+      alignItems: "center",
+      width: "70%",
+      marginTop:10,
+      borderColor:"black",
+      color:"black"
+  },
+  button : {
+      width: "90%",
+      borderColor:"black",
+      height: 50,
+      borderRadius:30,
+      padding:5,
+      alignItems: "center",
+      justifyContent:"center",
+  },
+  text: {
+      fontSize:14, 
+      color: "#fff",
+      fontWeight: "bold",
+      textAlign: "center"
   }
 });
