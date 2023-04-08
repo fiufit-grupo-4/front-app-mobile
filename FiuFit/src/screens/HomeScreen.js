6f8c50ebbd057bbd5356f6e263b4c511185b9f65
Dante Reinaudo <dantereinaudo@hotmail.com>
2023-04-08 16:26:04 -0300

    Home Screen

--- FiuFit/src/screens/HomeScreen.js
+++ FiuFit/src/screens/HomeScreen.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text,StyleSheet} from 'react-native';
+import CustomButton from '../components/CustomButton';
+import {useNavigation} from '@react-navigation/native';
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  const onLogOutPressed = () => {
+    // validate user
+    navigation.navigate('SignIn');
+  };
+
   return (
-    <View>
-      <Text style={{fontSize: 24, alignSelf: 'center'}}>Home, sweet home</Text>
+    <View style={styles.root}>
+      <Text style={styles.title}>Home, sweet home</Text>
+
+      <CustomButton
+          text="Log Out"
+          onPress={onLogOutPressed}
+          type="PRIMARY"
+      />
+
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    alignItems: 'center',
+    flex:1,
+    padding: 20,
+    backgroundColor:"skyblue",
+    justifyContent: "center"
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'white',
+    alignSelf: 'center',
+    margin: 10,
+    marginBottom:20
+  },
+});
 
 export default HomeScreen;
