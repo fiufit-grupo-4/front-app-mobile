e9ec6d48b075018740a14fa777756ecbc3e60aed
004559613 <tizzianamazza@gmail.com>
2023-04-19 11:30:13 -0300

    Home Tab

---
+++ FiuFit/src/screens/HomeTab.js
@@ -0,0 +1,8 @@
+import {useNavigation} from "@react-navigation/native";
+import ProfileScreen from "./profile/ProfileScreen";
+import {View, Text,StyleSheet,TouchableOpacity,Image} from 'react-native';
+
+export const HomeTab = () => {
+    return <Text>Holaa</Text>
+}
+
