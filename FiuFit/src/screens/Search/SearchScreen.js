153c23f55d688eea9fc28fc4a19847f54c5b8600
004559613 <tizzianamazza@gmail.com>
2023-04-20 17:52:32 -0300

    New SearchScreen, with filters

--- FiuFit/src/screens/Search/SearchScreen.js
+++ FiuFit/src/screens/Search/SearchScreen.js
@@ -1,38 +1,106 @@
-import React from 'react';
-import {Button, Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-
+import React, { useState } from 'react';
+import { View, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import {createMaterialTopTabNavigator} from "@react-navigation/material-top-tabs"
-import styles from "../../styles/styles";
-
-const Tab = createMaterialTopTabNavigator();
-
 
 export const SearchScreen = () => {
-        return (
-            <View style={searchStyle.container}>
-                <View style={searchStyle.buttonContainer}>
-                    <Button title="Trainers"/>
-                </View>
-                <View style={searchStyle.buttonContainer}>
-                    <Button title="Training"/>
-                </View>
+    const [searchText, setSearchText] = useState('');
+    const [filter, setFilter] = useState(null);
+
+    const handleSearch = () => {
+        // Handle search logic here
+        console.log('Searching for:', searchText, 'with filter:', filter);
+    };
+
+    const handleClear = () => {
+        // Clear search text and filter
+        setSearchText('');
+        setFilter(null);
+    };
+
+    const handleFilter = (filterValue) => {
+        // Set the selected filter
+        setFilter(filterValue);
+    };
+
+    return (
+        <View style={styles.searchContainer}>
+            <View style={styles.searchBox}>
+                <MaterialCommunityIcons name="magnify" color='gray' size={26} />
+                <TextInput
+                    placeholder="Search"
+                    placeholderTextColor="#999"
+                    style={styles.searchInput}
+                    value={searchText}
+                    onChangeText={(text) => setSearchText(text)}
+                />
+                <TouchableOpacity onPress={handleClear} style={styles.clearButton}>
+                    <Icon name="times" size={10} style={{ color: '#999' }} />
+                </TouchableOpacity>
             </View>
-        );
-}
+            <View style={styles.filterButtons}>
+                <TouchableOpacity onPress={() => handleFilter('trainings')}>
+                    <Text style={filter === 'trainings' ? styles.activeFilterText : styles.filterText}>
+                        Trainings
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => handleFilter('trainers')}>
+                    <Text style={filter === 'trainers' ? styles.activeFilterText : styles.filterText}>
+                        Trainers
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
 
-
-const searchStyle = StyleSheet.create({
-    container: {
-        flex: 1,
+const styles = StyleSheet.create({
+    searchContainer: {
         flexDirection: 'row',
-        alignItems: 'flex-start',
-        justifyContent: 'center'
+        alignItems: 'center',
+        backgroundColor: '#f0f0f0',
+        borderRadius: 5,
+        marginHorizontal: 10,
+        marginVertical: 5,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
     },
-    buttonContainer: {
+    searchBox: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        borderRadius: 5,
         flex: 1,
-        borderRadius: 4,
-        paddingVertical: 3,
-        color: '#ffffff'
-    }
+        paddingHorizontal: 10,
+    },
+    searchInput: {
+        fontSize: 16,
+        fontWeight: '400',
+        color: '#333',
+        flex: 1,
+        marginLeft: 10,
+    },
+    clearButton: {
+        marginLeft: 10,
+    },
+    filterButtons: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 10,
+    },
+    filterText: {
+        fontSize: 16,
+        fontWeight: '400',
+        color: '#999',
+        marginLeft: 10,
+    },
+    activeFilterText: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#333',
+        marginLeft: 10,
+    },
 });
+
+export default SearchScreen;
